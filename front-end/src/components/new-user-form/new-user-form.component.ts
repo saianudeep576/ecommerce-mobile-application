2be48132fcfc96aa67afefba81e744a24e52697b
saianudeep576 <saianudeeppalagummi@gmail.com>
2024-09-08 18:32:42 +0530

    new user form

--- front-end/src/components/new-user-form/new-user-form.component.ts
+++ front-end/src/components/new-user-form/new-user-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-user-form',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-user-form.component.scss']
 })
 export class NewUserFormComponent {
-
+  newUserForm: FormGroup;
+  genders = ['Male', 'Female', 'Prefer not to say']
+  constructor(public fb: FormBuilder) {
+    this.newUserForm = this.fb.group({
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      genderSelection: new FormControl('', Validators.required),
+      age: new FormControl('', Validators.required),
+      email: new FormControl('',Validators.required)
+    })
+  }
 }
