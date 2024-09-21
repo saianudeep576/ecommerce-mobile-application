02ad81bcd86cd4fcb6d6ee20d7877b079343c07b
unknown <saianudeeppalagummi@gmail.com>
2024-09-21 18:23:23 +0530

    connecting db and backend

---
+++ backend/phone-backend.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql");
+const app = express();
+
+//establishing database connection
+const database = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Sai@1999",
+  database: "ecommerce", //need to mention the schema name
+});
+
+//checking database connection status
+database.connect((error) => {
+  if (error) {
+    console.log("error connecting to DB");
+  } else {
+    console.log("successfully connected to DB");
+  }
+});
+
+//getting the records
+app.get("/getPhoneInfo", (req, res) => {
+  const command = "SELECT * FROM PHONES"; //command for fetching the records, here we need to mention the table name
+  database.query(command, (error, result) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.listen(3001, () => {
+  console.log("successfully backend is up");
+});
